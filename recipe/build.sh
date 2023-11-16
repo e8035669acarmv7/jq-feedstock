d348ba8ff003ed253949ea01dc2e83e38ffd7f24
Bela Stoyan <bela.stoyan@gmail.com>
2023-11-16 22:49:58 +0100

    --disable-maintainer-mode is required for 1.6

--- recipe/build.sh
+++ recipe/build.sh
@@ -9,7 +9,7 @@ cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
 
 chmod +x configure
 
-./configure --prefix=$PREFIX --with-oniguruma=$PREFIX
+./configure --disable-maintainer-mode --prefix=$PREFIX --with-oniguruma=$PREFIX
 
 make -j${CPU_COUNT}
 if [[ "${CONDA_BUILD_CROSS_COMPILATION}" != "1" ]]; then
