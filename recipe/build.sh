88b0fdbbce2ca74a012e771216ca8bd1a45317d8
Bela Stoyan <bela.stoyan@gmail.com>
2023-11-16 17:03:49 +0100

    debug

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+set +e
+set -x
+
 # Get an updated config.sub and config.guess
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./modules/oniguruma
 cp $BUILD_PREFIX/share/gnuconfig/config.* ./config
